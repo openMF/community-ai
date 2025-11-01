78224a9528be4b883523ac9f3afd88ba8a39d531
Learning <sahilgreat321@gmail.com>
2025-11-01 18:02:03 +0530

    chore(docs): remove tests and CI workflow from README PR per maintainer request

--- tests/test_files_exist.py
+++
@@ -1,5 +0,0 @@
-﻿import os
-
-def test_core_files_exist():
-    assert os.path.isfile('README.md'), 'README.md must exist'
-    assert os.path.isfile('CodeCommentingScript.py'), 'CodeCommentingScript.py must exist'
